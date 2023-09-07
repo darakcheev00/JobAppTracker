5b7b8a02ec8117340c155f19f12804ef2a424dde
darakcheev00 <daniel.arakcheev@gmail.com>
2023-09-07 15:38:13 -0400

    disabled api key

--- src/gptmodule.ts
+++ src/gptmodule.ts
@@ -1,5 +1,5 @@
 
-const API_KEY = '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';
+const API_KEY = '***';
 
 const context = "You are a job application email analyzer. The emails are sent from the employer that I have applied to. The emails i provide to you are in the format: gptInput = {'sender':msg['sender'],'subject':msg['subject'],'body':msg['body']}. Read this email and output these things: Company name and position name. (If not found then write 'unspecified') Also select which status option describes the email best. Status options: ['application received', 'rejected', 'interview requested', 'received offer','not related to job application','invited to apply']. Gpt output format is a json string containing these keys: 'company', 'position', 'status'. If you determine the position to be unspecified, check again. Rules: Use double quotes for strings in the json string! Remember the '{' and '}' in the json string!. ONLY RETURN THE json string! Do not add any other text! Email: "
 
