1402dfa5e11cb33625ffea6a90de1b96459bd998
Daniel Arakcheev <46634994+darakcheev00@users.noreply.github.com>
2023-09-10 17:42:18 -0400

    Update GptForm.tsx

--- src/GptForm.tsx
+++ src/GptForm.tsx
@@ -9,20 +9,27 @@ type GptSubmitForm = {
 type GptFormProps = {
     setGptKey: (key: string) => void; // Define the type for setGptKey
     setGptKeyValid: (key: boolean) => void;
+    setRefreshMsg: (key: string) => void;
 };
 
-export default function GptForm({ setGptKey, setGptKeyValid }: GptFormProps) {
+export default function GptForm({ setGptKey, setGptKeyValid, setRefreshMsg}: GptFormProps) {
     const { register, handleSubmit, reset } = useForm<GptSubmitForm>();
 
     const handleGptKeySubmit  = async(data: GptSubmitForm) => {
         console.log(`Submitted key: ${data.gptkeyInput}`);
-
+        let displayMsg = "Invalid key! Try again.";
         if (await GptManager.healthCheck(data.gptkeyInput)) {
             setGptKey(data.gptkeyInput);
             await StorageManager.setGptKey(data.gptkeyInput);
             setGptKeyValid(true);
+            displayMsg = "Valid key!";
             console.log(await StorageManager.getGptKey());
         }
+
+        setRefreshMsg(displayMsg);
+		setTimeout(() => {
+			setRefreshMsg("")
+		}, 4000);
 
         reset({
             gptkeyInput: '',
